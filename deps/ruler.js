a6c0ccc3c6e6a93fda496fbfbedb89991b9be86c
Vlad <vkrokh@gmail.com>
2017-05-03 17:37:43 +0300

    fix

--- deps/ruler.js
+++ deps/ruler.js
@@ -176,8 +176,8 @@ window.onload = function () {
             ctx.strokeStyle = 'blue';
             ctx.strokeText(x1 + '\u00D7' + y1 + ' (' + dx + '\u00D7' + dy + ')', x1 + 15, y1 - 10);
             var lengthValue = document.getElementById('coordinateName').value;
-            document.getElementById('position').value = x1 + '\u00D7' + y1;
-            document.getElementById('len').value = (0.007 * (lengthValue + (dy / 192000))) / (dy / 192000) + ' M';
+            document.getElementById('position').value = lengthValue;
+            document.getElementById('len').value = (0.007 * (1.7 + (dy / 192000))) / (dy / 192000) + ' M';
             document.getElementById('size').value = dx + '\u00D7' + dy;
         }
     };
